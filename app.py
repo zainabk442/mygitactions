f8bbe97712cbd428c981bf3d86d9d3e85171c2d2
zainab <zk758166@gmail.com>
2026-03-26 17:33:47 +0530

    Resolved merge conflict in app.py

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-<<<<<<< HEAD
+# <<<<<<< HEAD
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def home():
+#     return "Hello from Docker + GitHub Actions + AWS!"
+#
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+# =======
+# print("Hello from GitHub Actions pipeline!")
+# >>>>>>> f2eb77dc0c68e8c70b4145023e92e1a828633e8f
+
+
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,6 +25,3 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-=======
-print("Hello from GitHub Actions pipeline!")
->>>>>>> f2eb77dc0c68e8c70b4145023e92e1a828633e8f
